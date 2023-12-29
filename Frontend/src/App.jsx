20c51eb8b18c33252893b4d607d52d105d7ec23c
Abhishek k <bobbyyyyy.16@gmail.com>
2023-12-29 19:50:57 +0530

    basic routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,17 @@
+import {Routes, Route} from 'react-router-dom'
+import HomePage from './Pages/HomePage'
+import CreatePage from './Pages/CreatePage'
+
 
 function App() {
-
-  return null
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/create' element={<CreatePage />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App
---
+++ Frontend/src/Pages/CreatePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CreatePage() {
+  return (
+    <div>
+      <h1>This is create page</h1>
+    </div>
+  )
+}
